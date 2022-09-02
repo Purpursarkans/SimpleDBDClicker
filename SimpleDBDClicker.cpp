05a2486e30596a805c3e060433b6daf5596634d9
Purpursarkans <goga.danilov10@gmail.com>
2022-09-02 22:15:11 +0400

    update

--- SimpleDBDClicker.cpp
+++ SimpleDBDClicker.cpp
@@ -7,6 +7,37 @@ static int HOST_ERROR_CLICK_Y = 449;
 static int CONTINUE_X = 1226;
 static int CONTINUE_Y = 715;
 
+int ToSec(int sec);
+int ToMin(int min);
+
+struct RGBPixel
+{
+    int r;
+    int g;
+    int b;
+};
+
+void FindColor(int x, int y, int r, int g, int b, HDC DC)
+{
+    COLORREF color;
+    RGBPixel Pixel;
+    while (!(Pixel.r > r && Pixel.g < g && Pixel.b < b))
+    {
+        color = GetPixel(DC, x, y);
+        Pixel = {GetRValue(color), GetGValue(color), GetBValue(color)};
+        std::cout << int(GetRValue(color)) << " " << int(GetGValue(color)) << " " << int(GetBValue(color)) << std::endl;
+        Sleep(ToSec(1));
+        for(int i = 15; i > 0; i--)
+        {
+            system("cls");
+            std::cout << "time remaining: " << i << " sec" << std::endl;
+            Sleep(ToSec(1));
+        }
+    }
+    Beep(100, 100);
+    Sleep(ToSec(3));
+}
+
 void LClickOnCoord(int X, int Y)
 {
     SetCursorPos(X, Y);
@@ -17,19 +48,19 @@ void LClickOnCoord(int X, int Y)
 
 int ToSec(int sec)
 {
-    return sec*1000;
+    return sec * 1000;
 }
 
 int ToMin(int min)
 {
-    return min*60000;
+    return min * 60000;
 }
 
 int main()
 {
-    
     HWND GameHWND = NULL;
     GameHWND = FindWindowA("UnrealWindow", NULL);
+    HDC GameDC = GetDC(GameHWND);
 
     MoveWindow(GetConsoleWindow(), 0, 50, 255, 100, TRUE);
     SetWindowPos(GetConsoleWindow(), HWND_TOPMOST, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE);
@@ -41,39 +72,13 @@ int main()
     std::cout << "Sleep 5 sec" << std::endl;
     Sleep(ToSec(5));
 
-    for(int i = 1;;i++)
+    for (int i = 1;; i++)
     {
+        FindColor(1290, 714, 100, 10, 10, GameDC);
         SwitchToThisWindow(GameHWND, false);
         Sleep(ToSec(1));
         LClickOnCoord(CONTINUE_X, CONTINUE_Y);
-
-        for(int j = 16; j > 0; j--)
-        {
-            for(int n = 59; n > 0; n--)
-            {
-                system("cls");
-                std::cout << "done times: " << i << std::endl;
-                std::cout << "Sleep left: " << j << ":" << n << " min" << std::endl;
-                Sleep(ToSec(1));
-            }
-            
-        }
-
-        Beep(100,100);
-        Sleep(ToSec(3));
-
-        SwitchToThisWindow(GameHWND, false);
-        Sleep(ToSec(3));
-
-        LClickOnCoord(HOST_ERROR_CLICK_X, HOST_ERROR_CLICK_Y);
-        Sleep(ToSec(2));
-        LClickOnCoord(CONTINUE_X, CONTINUE_Y);
-        for (int j = 30; j > 0; j--)
-        {
-            system("cls");
-            std::cout << "Sleep left: " << j << " sec" << std::endl;
-            Sleep(ToSec(1));
-        }
+        Sleep(ToSec(1));
     }
     system("pause");
 }
