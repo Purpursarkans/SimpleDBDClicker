ae69c826d321ee905eba4f4ff9253047a0c3ae50
Purpursarkans <goga.danilov10@gmail.com>
2022-09-02 14:45:10 +0400

    update

--- SimpleDBDClicker.cpp
+++ SimpleDBDClicker.cpp
@@ -7,40 +7,6 @@ static int HOST_ERROR_CLICK_Y = 449;
 static int CONTINUE_X = 1226;
 static int CONTINUE_Y = 715;
 
-void simulateMouseClickOnWnd(HWND targetWnd, int clickX, int clickY, int clickType)
-{
-    HWND foreWnd = GetForegroundWindow();
-    DWORD windowThreadProcessId = GetCurrentThreadId();
-    DWORD wakfuThreadID = GetWindowThreadProcessId(targetWnd, 0);
-
-    AttachThreadInput(windowThreadProcessId, wakfuThreadID, true); // switch input to targetWnd and set focust on to it
-    SetFocus(targetWnd);
-
-    DWORD mousePos; // Im setting desired click position and send it throught lParam
-    *(WORD *)&mousePos = clickX;
-    *((WORD *)&mousePos + 1) = clickY;
-
-    if (clickType == 0) //        LEFT CLICK
-    {
-        SendMessage(targetWnd, WM_MOUSEMOVE, 0, mousePos);
-        SendMessage(targetWnd, WM_LBUTTONDOWN, MK_LBUTTON, mousePos);
-        Sleep(50);
-        SendMessage(targetWnd, WM_MOUSEMOVE, 0, mousePos);
-        SendMessage(targetWnd, WM_LBUTTONUP, 0, mousePos);
-    }
-    else //                        RIGHT CLICK
-    {
-        SendMessage(targetWnd, WM_MOUSEMOVE, 0, mousePos);
-        SendMessage(targetWnd, WM_RBUTTONDOWN, MK_RBUTTON, mousePos);
-        Sleep(50);
-        SendMessage(targetWnd, WM_MOUSEMOVE, 0, mousePos);
-        SendMessage(targetWnd, WM_RBUTTONUP, 0, mousePos);
-    }
-
-    AttachThreadInput(windowThreadProcessId, wakfuThreadID, false);
-    SetFocus(foreWnd);
-}
-
 void LClickOnCoord(int X, int Y)
 {
     SetCursorPos(X, Y);
